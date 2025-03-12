effee24e6a1055b8749d3cecbcd67f7e7b549447
Prakash Bhatt <112472788+PrakashBhatt29@users.noreply.github.com>
2025-03-12 21:36:03 +0530

    Fixed Cors

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,41 @@
 import withAuth from "next-auth/middleware";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+
+const allowedOrigins = ['https://nutriai-fit.vercel.app', 'http://localhost:3000']
+
+const corsOptions = {
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
 
 export default withAuth(
-    function middleware(req){
-        return NextResponse.next()
+    function middleware(request: NextRequest ){
+        const origin = request.headers.get('origin') ?? ''
+        const isAllowedOrigin = allowedOrigins.includes(origin)
+       
+        // Handle preflighted requests
+        const isPreflight = request.method === 'OPTIONS'
+       
+        if (isPreflight) {
+          const preflightHeaders = {
+            ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+            ...corsOptions,
+          }
+          return NextResponse.json({}, { headers: preflightHeaders })
+        }
+       
+        // Handle simple requests
+        const response = NextResponse.next()
+       
+        if (isAllowedOrigin) {
+          response.headers.set('Access-Control-Allow-Origin', origin)
+        }
+       
+        Object.entries(corsOptions).forEach(([key, value]) => {
+          response.headers.set(key, value)
+        })
+       
+        return response
     },
     {
         callbacks:{
@@ -25,5 +57,5 @@ export default withAuth(
 )
 
 export const config = {
-    matcher: ["/((?!_next/static|_next/image|favicon.ico|public/).*)"]
+    matcher: ["/((?!_next/static|_next/image|favicon.ico|public/).*)", "/api/:path*"],
 }
